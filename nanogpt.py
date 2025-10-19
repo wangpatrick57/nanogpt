8a734c7a69d260339ffdf5bd215b6650fc674b5b
Patrick Wang <wang.patrick57@gmail.com>
2025-10-19 15:26:45 -0700

    vocab

--- nanogpt.py
+++ nanogpt.py
@@ -3,13 +3,13 @@ from pathlib import Path
 import wget
 
 
-def set_up_train_data() -> None:
+if __name__ == "__main__":
     TRAIN_DATA_URL = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
     TRAIN_DATA_PATH = Path("input.txt")
 
     if not TRAIN_DATA_PATH.exists():
         wget.download(TRAIN_DATA_URL)
-
-
-if __name__ == "__main__":
-    set_up_train_data()
+    raw_train_data_text = open(TRAIN_DATA_PATH).read()
+    enc = {c: i for i, c in enumerate(sorted(list(set(raw_train_data_text))))}
+    dec = {i: c for c, i in enc.items()}
+    vocab_size = len(enc)
