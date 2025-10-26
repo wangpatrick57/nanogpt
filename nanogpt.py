8f279820d5b9a2414dc95e151ad4201438749732
Patrick Wang <wang.patrick57@gmail.com>
2025-10-25 17:14:11 -0700

    added transformer head

--- nanogpt.py
+++ nanogpt.py
@@ -34,56 +34,104 @@ class DataLoader:
     def get_batch(self, split: DataSplit) -> tuple[torch.Tensor, torch.Tensor]:
         data = self.data_map[split]
         batch_idxs = torch.randint(len(data) - self.context_length, (self.batch_size,))
-        X = torch.stack(
+        x = torch.stack(
             [
                 data[batch_idx : batch_idx + self.context_length]
                 for batch_idx in batch_idxs
             ]
         )
-        Y = torch.stack(
+        y = torch.stack(
             [
                 data[batch_idx + 1 : batch_idx + self.context_length + 1]
                 for batch_idx in batch_idxs
             ]
         )
-        return X, Y
+        return x, y
 
 
-class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size: int):
+class AttentionHead(nn.Module):
+    def __init__(self, embed_dim: int, head_dim: int):
         super().__init__()
-        self.embed = nn.Embedding(vocab_size, vocab_size)
+        self.query = nn.Linear(embed_dim, head_dim, bias=False)
+        self.key = nn.Linear(embed_dim, head_dim, bias=False)
+        self.value = nn.Linear(embed_dim, head_dim, bias=False)
+        # TODO: figure out whether this needs to be a buffer
+        self.tril = torch.tril(torch.ones(context_length, context_length))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        x is (batch_size, context_size, embed_dim)
+
+        Returns out
+        out is (batch_size, context_size, embed_dim)
+        """
+        context_size = x.shape[-2]
+        q = self.query(x)
+        k = self.key(x)
+        v = self.value(x)
+        logits = q @ k.transpose(-1, -2) / embed_dim**0.5
+        masked_logits = logits.masked_fill(
+            self.tril[:context_size, :context_size] == 0, float("-inf")
+        )
+        probs = F.softmax(masked_logits, dim=-1)
+        out = probs @ v
+        return out
+
+
+class TransformerLanguageModel(nn.Module):
+    def __init__(
+        self, vocab_size: int, context_length: int, embed_dim: int, head_dim: int
+    ):
+        super().__init__()
+        self.context_length = context_length
+        self.token_emb = nn.Embedding(vocab_size, embed_dim)
+        self.pos_emb = nn.Embedding(context_length, embed_dim)
+        self.attn_head = AttentionHead(embed_dim, head_dim)
+        self.lm_head = nn.Linear(embed_dim, vocab_size)
 
     def forward(
-        self, X: torch.Tensor, Y: torch.Tensor | None = None
+        self, x: torch.Tensor, y: torch.Tensor | None = None
     ) -> tuple[torch.Tensor, torch.Tensor | None]:
-        logits = self.embed(X)
+        """
+        x is (batch_size, context_size, vocab_size)
+        y is (batch_size, context_size)
+
+        Returns logits, loss
+        logits is (batch_size, context_size, vocab_size)
+        loss is (1,)
+        """
+        token_embeds = self.token_emb(x)
+        pos_embeds = self.pos_emb(torch.arange(x.shape[1]))
+        embeds = token_embeds + pos_embeds
+        attn_out = self.attn_head(embeds)
+        logits = self.lm_head(attn_out)
         loss = (
             None
-            if Y is None
-            else F.cross_entropy(logits.view(-1, logits.shape[-1]), Y.view(-1))
+            if y is None
+            else F.cross_entropy(logits.view(-1, logits.shape[-1]), y.view(-1))
         )
         return logits, loss
 
-    def generate(self, contexts: torch.Tensor, max_new_tokens: int) -> torch.Tensor:
+    def generate(self, texts: torch.Tensor, max_new_tokens: int) -> torch.Tensor:
         for _ in range(max_new_tokens):
+            contexts = texts[:, -self.context_length :]
             logits, _ = self(contexts)
             new_logits = logits[:, -1, :]
             probs = F.softmax(new_logits, dim=-1)
             tokens = torch.multinomial(probs, 1)
-            contexts = torch.cat((contexts, tokens), dim=-1)
-        return contexts
+            texts = torch.cat((texts, tokens), dim=-1)
+        return texts
 
 
 @torch.no_grad()
 def estimate_losses(
-    model: BigramLanguageModel, data_loader: DataLoader
+    model: TransformerLanguageModel, data_loader: DataLoader
 ) -> dict[DataSplit, float]:
     losses = dict()
     model.eval()
     for split in DataSplit:
-        X_batch, Y_batch = data_loader.get_batch(split)
-        _, loss = model(X_batch, Y_batch)
+        x_batch, y_batch = data_loader.get_batch(split)
+        _, loss = model(x_batch, y_batch)
         losses[split] = loss.item()
     model.train()
     return losses
@@ -98,13 +146,14 @@ if __name__ == "__main__":
     # Training hyperparameters.
     train_ratio = 0.9
     batch_size = 32
-    max_iters = 3000
-    eval_interval = 300
-    lr = 1e-2
+    max_iters = 5000
+    eval_interval = 500
+    lr = 1e-3
 
     # Model hyperparameters.
     context_length = 8
     embed_dim = 32
+    head_dim = embed_dim
 
     # Preprocess data.
     if not data_path.exists():
@@ -118,7 +167,7 @@ if __name__ == "__main__":
     data_loader = DataLoader(data, train_ratio, batch_size, context_length)
 
     # Train model.
-    model = BigramLanguageModel(vocab_size)
+    model = TransformerLanguageModel(vocab_size, context_length, embed_dim, head_dim)
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
     est_loss_iters = []
     losses_over_time = defaultdict(list)
@@ -130,8 +179,8 @@ if __name__ == "__main__":
             for split, loss in losses.items():
                 losses_over_time[split].append(loss)
 
-        X_batch, Y_batch = data_loader.get_batch(DataSplit.TRAIN)
-        _, loss = model(X_batch, Y_batch)
+        x_batch, y_batch = data_loader.get_batch(DataSplit.TRAIN)
+        _, loss = model(x_batch, y_batch)
         optimizer.zero_grad()
         assert isinstance(loss, torch.Tensor)
         loss.backward()
